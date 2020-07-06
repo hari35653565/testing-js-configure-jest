b73975d75a3fa21247c48739a9c811743ab230ec
Manassarn Manoonchai <manassarn@gmail.com>
2020-07-06 13:32:39 +0700

    :heavy_plus_sign: Use jestDOM testing library for better expect

--- src/__tests__/calculator.js
+++ src/__tests__/calculator.js
@@ -1,7 +1,21 @@
+import * as jestDOM from '@testing-library/jest-dom'
 import React from 'react'
-import {render} from '@testing-library/react'
+import {render, fireEvent} from '@testing-library/react'
 import Calculator from '../calculator'
+
+expect.extend(jestDOM)
 
 test('renders', () => {
   render(<Calculator />)
+})
+
+test('the clear button switches from AC to C when there is an entry', () => {
+  const { getByText } = render(<Calculator />)
+  const clearButton = getByText('AC')
+
+  fireEvent.click(getByText(/3/))
+  expect(clearButton).toHaveTextContent('C')
+
+  fireEvent.click(clearButton)
+  expect(clearButton).toHaveTextContent('AC')
 })
