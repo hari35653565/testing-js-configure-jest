865f73d2170fa794618f3341a2cc91dcccfb3838
Manassarn Manoonchai <manassarn@gmail.com>
2020-07-06 14:56:35 +0700

    :recycle: Refactor

--- src/shared/__tests__/calculator-display.js
+++ src/shared/__tests__/calculator-display.js
@@ -1,20 +1,6 @@
 import React from 'react'
-import PropTypes from 'prop-types'
-import {render as rtlRender} from '@testing-library/react'
-import {ThemeProvider} from 'emotion-theming'
-import {dark} from '../../themes'
+import {render} from '../../../test/calculator-test-utils'
 import CalculatorDisplay from '../calculator-display'
-
-function render(ui, options) {
-  return rtlRender(ui, { wrapper: Wrapper, ...options})
-}
-function Wrapper({children}) {
-  return <ThemeProvider theme={dark}>{children}</ThemeProvider>
-}
-
-Wrapper.propTypes = {
-  children: PropTypes.node,
-}
 
 test('renders', () => {
   const {container} = render(<CalculatorDisplay value="0" />)
